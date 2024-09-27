6a5519ed605ce9f9afc07c2aadc3bd8b446075ca
nipun <nipun@nipuns-MacBook-Air.local>
2024-09-27 17:23:12 -0500

    calculator

--- projects/calculator/js/calculator.js
+++ projects/calculator/js/calculator.js
@@ -11,19 +11,37 @@ var clearOnEntry;
  * 
  * @param {number} digit digit to add or display on the `outputScreen`
  */
-function enterDigit(digit) {}
+function enterDigit(digit) {
+    let display_element = document.querySelector("#result");
+    if (display_element.innerText == '0') { display_element.innerText = '' }
+    display_element.innerText += digit
+}
 
 
 /**
  * Clear `outputScreen` and set value to 0
  */
-function clear_screen() {}
+function clear_screen() {
+    let display_element = document.querySelector("#result");
+    display_element.innerText = 0
+}
 
 
 /**
  * Evaluate the expression and display its result or *ERROR*
  */
-function eval_expr() {}
+function eval_expr() {
+    let display_element = document.querySelector("#result")
+    let result;
+    try {
+        result = eval(display_element.innerText);
+    }
+    catch {
+        result = "ERROR"
+    }
+
+    display_element.innerText = result;
+}
 
 
 /**
@@ -31,7 +49,10 @@ function eval_expr() {}
  * 
  * @param {string} operation to add to the expression
  */
-function enterOp(operation) {}
+function enterOp(operation) {
+    let display_element = document.querySelector("#result");
+    display_element.innerText += operation
+}
 
 
 window.onload = function () {
