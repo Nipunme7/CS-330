029ca5c3c402a69e127e288a5d18a99ff81e765c
nipun <nipun@nipuns-MacBook-Air.local>
2024-11-08 12:12:54 -0600

    commit

--- projects/jokes_api/client/script.js
+++ projects/jokes_api/client/script.js
@@ -1,2 +1,77 @@
 'use strict';
 
+var BASE_URL = "https://Nipun007.pythonanywhere.com/api/v1/jokes/";
+
+function populate_select(element, list_to_populate_with) {
+    for (let item of list_to_populate_with) {
+        let option_element = document.createElement('option');
+        option_element.setAttribute("value", item)
+        option_element.innerText = item
+        element.append(option_element)
+    }
+}
+function disable_enable_id_element(element) {
+    if (element.value == 1) {
+        document.querySelector("#joke_id").disabled = false;
+    } else {
+        document.querySelector("#joke_id").disabled = true;
+        document.querySelector("#joke_id").value = "";
+    }
+
+}
+function fetch_jokes() {
+    // give error under certain conditions
+    let category_value = document.querySelector("#selCat").value
+    let lang_value = document.querySelector("#selLang").value;
+    let number_value = document.querySelector("#number").value;
+    let id_value = document.querySelector("#joke_id").value;
+    if (id_value == "") {
+        get_and_display_data(category_value, lang_value, number_value)
+    } else {
+        get_and_display_data(category_value, lang_value, number_value, id_value)
+    }
+}
+
+async function get_and_display_data(category_value, lang_value, number_value, id_value = null) {
+
+    var joke_div = document.querySelector("#jokes");
+    joke_div.innerHTML = "";
+    if (id_value == null) {
+        var fetch_url = BASE_URL + `${lang_value}\/${category_value}\/${number_value}`;
+    } else {
+        var fetch_url = BASE_URL + `${lang_value}\/${category_value}\/${number_value}\/${id_value}`;
+    }
+    fetch(fetch_url)
+        .then((response) => response.json())
+        .then((response) => {
+            let all_jokes = response.jokes
+            for (let joke of all_jokes) {
+                let p_element = document.createElement("p");
+                p_element.setAttribute("class", "alert alert-primary")
+                p_element.innerText = joke
+                joke_div.append(p_element);
+            }
+            if (all_jokes.length == 0) {
+                let p_element = document.createElement("p");
+                p_element.setAttribute("class", "alert alert-primary")
+                p_element.innerText = "No kidding!"
+                joke_div.append(p_element);
+            }
+        })
+        .catch((reject) => {
+            let p_element = document.createElement("p");
+            p_element.setAttribute("class", "alert alert-primary")
+            p_element.innerText = "ERROR fetching data"
+            joke_div.append(p_element);
+        }
+        )
+}
+
+window.onload = function () {
+    populate_select(document.querySelector("#selLang"), ['en', 'es', 'de'])
+    populate_select(document.querySelector("#selCat"), ['neutral', 'chuck', 'all'])
+    document.querySelector("#number").addEventListener("input", (e) => { disable_enable_id_element(e.target) })
+    document.querySelector("#number").value = 1;
+}
+
+
--- projects/jokes_api/server/app.py
+++ projects/jokes_api/server/app.py
@@ -2,3 +2,69 @@
 """
 jokes api
 """
+import random
+from typing import List, Union
+
+import pyjokes
+from flask import Flask, request, abort, Response
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/api/v1/jokes", methods=["GET"])
+@app.route("/api/v1/jokes/<string:lang>/<string:category>/<int:number>", methods =["GET"])
+@app.route("/api/v1/jokes/<string:lang>/<string:category>/<int:number>/<int:id>", methods =["GET"])
+def api_response(lang="", category="", number=0, id=None):
+    if request.path == '/api/v1/jokes':
+        # first implementation
+        dict_values = dict(request.args)
+        category = dict_values.get('category', 'all')
+        lang = dict_values.get("language", 'en')
+        number = int(dict_values.get("number", 1))
+        id = int(dict_values.get("id", None))
+
+    if lang not in ['en', 'es', 'de'] or \
+            category not in ['neutral', 'all', 'chuck']:
+        # raise 404 error if invalid category
+        abort(404)
+
+    if number != 1 and id is not None:
+        abort(404)
+    else:
+        jokes = get_joke(lang, category, number, id=id)
+
+    response_object = Response(json.dumps(jokes), status=200)
+    response_object.headers['Content-Type'] = 'application/json'
+    response_object.headers['Access-Control-Allow-Origin'] = '*'
+    return response_object
+
+
+def get_joke(language: str = "en", category: str = "all", number: int = 1,
+            id: Union[int, None] = None) -> List[dict]:
+    jokes = {'language':language,
+             'category':category,
+             'jokes':[],
+             'number':number}
+
+    if category == 'chuck' and language == 'es':
+        jokes['number'] = 0
+        return jokes
+    else:
+        all_jokes = pyjokes.get_jokes(language, category)
+        if id is not None and id >= len(all_jokes):
+            # id is not in the list, raise an error 
+            abort(404)
+        try:
+            indexes = random.sample(range(0,len(all_jokes)), k=number)
+        except ValueError:
+            indexes = range(0, len(all_jokes))
+            jokes['number'] = len(all_jokes)
+
+        if id is None:
+            for index in indexes:
+                jokes['jokes'].append(all_jokes[index])
+        else:
+            jokes['jokes'].append(all_jokes[id])
+
+    return jokes
